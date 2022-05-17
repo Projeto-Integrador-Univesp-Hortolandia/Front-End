de386356891bd3f988e405c5527602d99e972948
HatNoir <hatsune.mk@hotmail.com>
2022-05-17 20:45:20 -0300

    feature/ libs

--- univesp-frontend/src/app/app-routing.module.ts
+++ univesp-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: 'content', loadChildren: () => import('./content/content.module').then(m => m.ContentModule) }, { path: 'messeger', loadChildren: () => import('./messeger/messeger.module').then(m => m.MessegerModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
