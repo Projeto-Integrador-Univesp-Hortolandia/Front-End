dc3ee1a6ff2ece30dae2fef53f91bd8a19352288
HatNoir <hatsune.mk@hotmail.com>
2022-07-02 21:13:22 -0300

    feature: rota para o servidor oficial

--- univesp-frontend/src/environments/environment.prod.ts
+++ univesp-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiLocal: 'http://192.168.15.9:3000/',
-  apiASP: 'https://localhost:7130'
+  apiASP: 'http://pi-univesp.azurewebsites.net/api/'
 };
